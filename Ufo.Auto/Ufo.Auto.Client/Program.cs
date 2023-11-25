58b15769f0c2e21e5ec7eda64b23e29086373ece
kobe liu <490384184@qqcom>
2023-11-26 02:09:54 +0800

    ok

--- Ufo.Auto/Ufo.Auto.Client/Program.cs
+++ Ufo.Auto/Ufo.Auto.Client/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
+builder.Services.AddBootstrapBlazor();
 await builder.Build().RunAsync();
